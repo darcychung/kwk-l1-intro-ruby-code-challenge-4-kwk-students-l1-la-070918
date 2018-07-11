884cf35a1bd48f04d9dba9c1e43aa4e055a77d85
darcychung <darcycchung@gmail.com>
2018-07-11 23:49:01 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,6 +1,20 @@
 #write out your code here
-
+least_coins= {"quarters"=>.25, "dimes"=>.1, "nickels" => .05, "pennies"=> .01}
 def least_coins(cents)
+coin_types = {
+  :quarters=>0
+  :dimes=>0
+  :nickels=>0
+  :pennies=>0 
+  if 
+    elsif
+  else
+end
+dimes = cents/10 
+if 
+  elsif
+else
+end
 
 #Code your answer here!
 
